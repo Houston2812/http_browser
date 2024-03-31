08f69848c5ac58e80972a871ffea280a920df1f2
Huseyn Gambarov <qambarov2812@gmail.com>
2024-03-31 18:01:50 -0400

    Dependency graph handled

--- client.py
+++ client.py
@@ -11,11 +11,10 @@ from colorama import init as colorama_init
 BUF_SIZE = 4096
 colorama_init()
 
-def serialize_http_request(url):
+def serialize_http_request(host, port, url):
     msg = b''
     CRLF = "\r\n"
 
-    host, port, url = parse_url(server)
     
     msg += "GET ".encode() + url.encode() + ' '.encode() + "HTTP/1.1".encode() + CRLF.encode()
     msg += "HOST: ".encode() + host.encode() 
@@ -30,6 +29,70 @@ def serialize_http_request(url):
     msg += CRLF.encode() 
 
     return msg
+# http:\/\/(\w+):?([0-9]+)?(\/[\w\-\.]+[^#?\s]+)
+
+def parse_response(response):
+    dependency_table = {}
+
+    response = response.decode()
+
+    CRLF = "\r\n"
+
+    header, body = response.split(CRLF + CRLF)
+
+    dependencies = body.rstrip('\n').split('\n')
+
+    log(f"Dependencies: {dependencies}", 'debug')
+
+    for dependency in dependencies:
+        elements = dependency.split(',')
+        log(f"Elements: {elements}", 'debug')
+        if len(elements[1]) == 0:
+            dependency_table[elements[0].strip(',')] = []
+        else:
+            dependency_table[elements[1].strip(',')].append(elements[0].strip(','))
+
+    return dependency_table
+
+
+
+def send_request(url):
+    host, port, url_elements = parse_url(server)
+    
+    log(f"URL elements: {url_elements}", "debug")
+
+    dependency_url = "/" + url_elements[0] + "/dependency.csv"
+
+    dependency_request = serialize_http_request(host, port, dependency_url)
+    log(f"Dependency request: {dependency_request}", 'debug')
+
+    clientSock.send(dependency_request)
+    log("Dependency request is sent", "info")
+    
+    time.sleep(1)
+    dependency_response = get_response()  
+        
+    log(f"Server response: {dependency_response}", 'info')
+    
+    dependencies = parse_response(dependency_response)
+
+    log(f"Dependency table: {dependencies}", 'debug')
+    
+
+def get_response():
+    server_response = b''
+    try:
+        while True:
+            data = clientSock.recv(BUF_SIZE)
+            server_response += data
+
+            # if 0 bytes received, it means client stopped sending
+            if  len(data) == 0 and server_response != 0:
+                break
+    except BlockingIOError:
+        pass
+    
+    return data
 
 def log(msg, level):
     colors = {
@@ -54,13 +117,13 @@ def log(msg, level):
     print(f"{color}client.py - {level.upper()} - {sign} {msg}{Style.RESET_ALL}")
 
 def parse_url(url):
-    pattern = '^http:\/\/(\w+):?([0-9]+)?(\/[^#?\s]+)'
+    pattern = 'http:\/\/(\w+):?([0-9]+)?\/(\w+)*\/([^#?\s]+)'
 
     tokens = re.split(pattern, url)
 
     host = tokens[1]
     port = tokens[2]
-    url = tokens[3]
+    url = tokens[3:-1]
 
     if port == None:
         port = "80"
@@ -83,38 +146,12 @@ if __name__ == "__main__":
     except socket.error as err: 
         log("socket creation failed with error %s" %(err), 'error')
 
-    request = serialize_http_request(server)
-
-    log(f"Request: {request}", 'debug')
-
     host, port, _ = parse_url(server)
 
     clientSock.connect((host, int(port)))
     clientSock.setblocking(0)
 
-    clientSock.send(request)
-    log("Request is sent", "info")
-    
-    time.sleep(1)
-    server_response = b''
-
-    try:
-        try:
-            while True:
-                data = clientSock.recv(BUF_SIZE)
-                server_response += data
-
-                # if 0 bytes received, it means client stopped sending
-                if  len(data) == 0 and server_response != 0:
-                    break
-        except BlockingIOError:
-            pass
-
-    except KeyboardInterrupt:
-        log(f"Finished", "info")
-        
-    log(f"Server response: {server_response}", 'info')
-    
+    request = send_request(server)
 
 
 

702cb0a5dc63a3e4924ea4789ba7e1a5ed39327a
Huseyn Gambarov <qambarov2812@gmail.com>
2024-04-01 04:25:19 +0000

    File creation handled

--- client.py
+++ client.py
@@ -15,7 +15,6 @@ def serialize_http_request(host, port, url):
     msg = b''
     CRLF = "\r\n"
 
-    
     msg += "GET ".encode() + url.encode() + ' '.encode() + "HTTP/1.1".encode() + CRLF.encode()
     msg += "HOST: ".encode() + host.encode() 
 
@@ -31,16 +30,14 @@ def serialize_http_request(host, port, url):
     return msg
 # http:\/\/(\w+):?([0-9]+)?(\/[\w\-\.]+[^#?\s]+)
 
-def parse_response(response):
+def parse_dependency_response(body):
     dependency_table = {}
 
-    response = response.decode()
+    
+    log(f'Body: {body}', 'warning')
+    create_file('dependency.csv', body)
 
-    CRLF = "\r\n"
-
-    header, body = response.split(CRLF + CRLF)
-
-    dependencies = body.rstrip('\n').split('\n')
+    dependencies = body.strip('\r\n').split('\n')
 
     log(f"Dependencies: {dependencies}", 'debug')
 
@@ -48,20 +45,34 @@ def parse_response(response):
         elements = dependency.split(',')
         log(f"Elements: {elements}", 'debug')
         if len(elements[1]) == 0:
-            dependency_table[elements[0].strip(',')] = []
+            key = elements[0].strip(',')
+            dependency_table[key] = []
         else:
-            dependency_table[elements[1].strip(',')].append(elements[0].strip(','))
+            key = elements[1].strip(',')
+            value = elements[0].strip(',')
+            
+            dependency_table[key].append(value)
+            dependency_table[value] = []
 
     return dependency_table
 
+def create_file(filename, data, bytes=False):
+    flags = ''
 
+    if bytes:
+        flags = 'wb'
+    else:
+        flags = 'w'
+
+    with open("proj/" + filename, flags) as file:
+        file.write(data)
+
+    log(f'File: {filename} is created', 'info')
 
 def send_request(url):
-    host, port, url_elements = parse_url(server)
-    
-    log(f"URL elements: {url_elements}", "debug")
+    host, port = parse_url(server)
 
-    dependency_url = "/" + url_elements[0] + "/dependency.csv"
+    dependency_url = "/dependency.csv"
 
     dependency_request = serialize_http_request(host, port, dependency_url)
     log(f"Dependency request: {dependency_request}", 'debug')
@@ -70,20 +81,62 @@ def send_request(url):
     log("Dependency request is sent", "info")
     
     time.sleep(1)
-    dependency_response = get_response()  
+    header, body = get_response()
         
-    log(f"Server response: {dependency_response}", 'info')
+    log(f"Server response: {header}", 'info')
     
-    dependencies = parse_response(dependency_response)
+    dependencies = parse_dependency_response(body)
 
     log(f"Dependency table: {dependencies}", 'debug')
-    
 
-def get_response():
+    index_url = '/' + list(dependencies.keys())[0]
+    log(f"Root site: {index_url}", 'info')
+    
+    index_request = serialize_http_request(host, port, index_url)
+    log(f"Index request: {index_request}", 'debug')
+
+    clientSock.send(index_request)
+    log("Index request is sent", "info")
+
+    time.sleep(0.2)
+    header, body = get_response()
+
+    create_file(list(dependencies.keys())[0], body)
+
+    for key, values in dependencies.items():
+        for value in values:
+
+            sub_url = '/' + value
+            
+            log(f"Sub url:  {sub_url}", 'debug')
+
+            sub_request = serialize_http_request(host, port, sub_url)
+            log(f"Sub request: {sub_request}", 'debug')
+
+            clientSock.send(sub_request)
+            log("Sub request is sent", 'info')
+
+            time.sleep(0.5)
+            byte_flag = False
+            if '.png' in value:
+                header, body = get_response(decode=False)
+                byte_flag = True
+            else:
+                header, body = get_response()
+            
+            create_file(value, body, bytes=byte_flag)
+
+
+def get_response(decode=True):
     server_response = b''
+    header = b''
+    body = b''
+
     try:
         while True:
             data = clientSock.recv(BUF_SIZE)
+
+
             server_response += data
 
             # if 0 bytes received, it means client stopped sending
@@ -92,7 +145,17 @@ def get_response():
     except BlockingIOError:
         pass
     
-    return data
+    index = server_response.find(b'\x0d\x0a\x0d\x0a')
+    
+    log(f'Parse index: {index}', 'debug')
+    header = server_response[:index]
+    body = server_response[index+4:]
+
+    if not decode:
+        log(f"Body: {body}", 'debug')
+        return header.decode(), body
+    else:
+        return header.decode(), body.decode()
 
 def log(msg, level):
     colors = {
@@ -117,18 +180,18 @@ def log(msg, level):
     print(f"{color}client.py - {level.upper()} - {sign} {msg}{Style.RESET_ALL}")
 
 def parse_url(url):
-    pattern = 'http:\/\/(\w+):?([0-9]+)?\/(\w+)*\/([^#?\s]+)'
-
+    # pattern = 'http:\/\/(\w+):?([0-9]+)?\/(\w+)*\/([^#?\s]+)'
+    pattern = '(\w+):?([0-9]+)?'
     tokens = re.split(pattern, url)
 
+    log(f"Tokens: {tokens}", 'debug')
     host = tokens[1]
     port = tokens[2]
-    url = tokens[3:-1]
 
     if port == None:
         port = "80"
     
-    return (host, port, url)
+    return (host, port)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Client.py")
@@ -146,7 +209,7 @@ if __name__ == "__main__":
     except socket.error as err: 
         log("socket creation failed with error %s" %(err), 'error')
 
-    host, port, _ = parse_url(server)
+    host, port = parse_url(server)
 
     clientSock.connect((host, int(port)))
     clientSock.setblocking(0)
